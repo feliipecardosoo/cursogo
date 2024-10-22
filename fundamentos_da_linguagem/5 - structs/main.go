036cff73337ad43b5c506c43b002b7a3cf0eec8f
feliipecardosoo <fglibino@gmail.com>
2024-10-22 12:29:32 -0300

    first commit

--- fundamentos_da_linguagem/5 - structs/main.go
+++ fundamentos_da_linguagem/5 - structs/main.go
@@ -10,4 +10,10 @@ type usuario struct {
 func main() {
 	var u usuario
 	fmt.Println(u)
+
+	usuario2 := usuario{"Felipe", 21}
+	fmt.Println(usuario2)
+
+	usuario3justName := usuario{idade: 20}
+	fmt.Println(usuario3justName)
 }
