d8e696b5fddf7e8544f877de8b325f350185bc5e
feliipecardosoo <fglibino@gmail.com>
2024-10-22 12:22:20 -0300

    first commit

--- fundamentos_da_linguagem/5 - structs/main.go
+++ fundamentos_da_linguagem/5 - structs/main.go
@@ -2,17 +2,12 @@ package main
 
 import "fmt"
 
-func calculosMatematicos(n1, n2 int) (int, int) {
-	somar := n1 + n2
-	subtrair := n1 - n2
-	return somar, subtrair
+type usuario struct {
+	nome  string
+	idade uint8
 }
 
 func main() {
-	// Tenho que declarar 2 variavies pois estou recebendo 2 retornos kk....
-	// se eu não quiser usar uma variavel posso substituir por _
-	somaCal, _ := calculosMatematicos(2, 4)
-	somaCal2, subCal2 := calculosMatematicos(2, 4)
-	fmt.Println(somaCal)
-	fmt.Println(somaCal2, subCal2)
+	var u usuario
+	fmt.Println(u)
 }
