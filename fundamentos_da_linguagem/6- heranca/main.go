7b497126bf9aa25fe45d4e8d7e524f569fb2a2d5
feliipecardosoo <fglibino@gmail.com>
2024-10-29 18:13:16 -0300

    first commit

--- fundamentos_da_linguagem/6- heranca/main.go
+++ fundamentos_da_linguagem/6- heranca/main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+type pessoa struct {
+	nome      string
+	sobrenome string
+	idade     uint8
+	altura    uint8
+}
+
+type estudande struct {
+	pessoa
+	curso     string
+	faculdade string
+}
+
 func main() {
 	fmt.Println("Struct")
+
+	p1 := pessoa{"Felipe", "Cardoso", 21, 170}
+	fmt.Println(p1)
+
+	e1 := estudande{p1, "ads", "estacio"}
+	fmt.Println(e1)
 }
