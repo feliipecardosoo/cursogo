bd476dd38fc48489b1f13381babdf0449bdabad5
feliipecardosoo <fglibino@gmail.com>
2024-11-06 21:33:21 -0300

    first commit

--- alura-1/aulabasica/main.go
+++ alura-1/aulabasica/main.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var nome string
-	fmt.Scanf("%s", &nome)
+	// var nome string
+	// fmt.Scanf("%s", &nome)
 
-	fmt.Println(nome)
+	// fmt.Println(nome)
 
+	var nomeUm string = "Felipe"
+	var nome2 *string
+	nome2 = &nomeUm
+
+	fmt.Println(*nome2, nomeUm)
+
+	nomeUm = "Joao"
+
+	fmt.Println(*nome2, nomeUm)
 }
