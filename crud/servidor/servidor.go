4dea55229604b1aff0d2115f21fe70be4c5356b8
feliipecardosoo <fglibino@gmail.com>
2024-11-08 15:39:05 -0300

    first commit

--- crud/servidor/servidor.go
+++ crud/servidor/servidor.go
@@ -59,7 +59,6 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 
 // BuscarUsuarios retorna todos os usuarios do banco
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
-	// Conectar ao banco de dados
 	db, erro := banco.Conectar()
 	if erro != nil {
 		http.Error(w, "Erro ao conectar no banco de dados", http.StatusInternalServerError)
@@ -67,7 +66,6 @@ func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	// Consulta para buscar todos os usuários
 	linhas, erro := db.Query("SELECT id, nome FROM usuarios")
 	if erro != nil {
 		http.Error(w, "Erro ao buscar os usuários", http.StatusInternalServerError)
@@ -75,10 +73,8 @@ func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 	}
 	defer linhas.Close()
 
-	// Criando uma slice para armazenar os resultados
 	var usuarios []usuario
 
-	// Iterando sobre os resultados da query
 	for linhas.Next() {
 		var usuario usuario
 		if erro := linhas.Scan(&usuario.ID, &usuario.Nome); erro != nil {
@@ -88,17 +84,14 @@ func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 		usuarios = append(usuarios, usuario)
 	}
 
-	// Verifica se houve erro durante a iteração
 	if erro := linhas.Err(); erro != nil {
 		http.Error(w, "Erro ao processar os dados", http.StatusInternalServerError)
 		return
 	}
 
-	// Configura o cabeçalho da resposta para JSON
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 
-	// Retorna os usuários como JSON
 	if erro := json.NewEncoder(w).Encode(usuarios); erro != nil {
 		http.Error(w, "Erro ao enviar os dados", http.StatusInternalServerError)
 	}
