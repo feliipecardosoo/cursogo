abafc000b1c50721bb3e7f36433111a7b8902789
feliipecardosoo <fglibino@gmail.com>
2024-11-07 22:09:20 -0300

    first commit

--- crud/main.go
+++ crud/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
+
+	router := mux.NewRouter()
+
+	fmt.Println("Server rodando na porta 5k")
+	log.Fatal(http.ListenAndServe(":5000", router))
 
 }
