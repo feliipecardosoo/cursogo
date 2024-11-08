6e5fcdf819aafdd3b0a4ae03cb59cbdb75cbf24f
feliipecardosoo <fglibino@gmail.com>
2024-11-07 23:07:41 -0300

    first commit

--- crud/servidor/servidor.go
+++ crud/servidor/servidor.go
@@ -3,6 +3,7 @@ package servidor
 import (
 	"crud/banco"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -31,4 +32,28 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	if erro != nil {
 		w.Write([]byte("Erro ao conectar no banco de dados"))
 	}
+	defer db.Close()
+
+	statement, erro := db.Prepare("insert into usuarios (nome) values (?)")
+	if erro != nil {
+		w.Write([]byte("Erro ao criar o statement"))
+		return
+	}
+	defer statement.Close()
+
+	insercao, erro := statement.Exec(usuario.Nome)
+	if erro != nil {
+		w.Write([]byte("Erro ao criar o statement/ insert"))
+		return
+	}
+
+	iduser, erro := insercao.LastInsertId()
+	if erro != nil {
+		w.Write([]byte("Erro ao obter ID inserido"))
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("ID: %d", iduser)))
+
 }
